4c324645e8c8c833745506f590df7a97d9667bae
Oliver Lidberg <oliverlidberg@gmail.com>
2025-10-08 14:11:45 +0200

    Added Test Class Olli

---
+++ Olli.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HejHej
+{
+    internal class Olli
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Hej");
+        }
+    }
+}
